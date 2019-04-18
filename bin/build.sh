59bfa63b49f44e81e227940e05f7452f349b1839
Jon Petto <jpetto@users.noreply.github.com>
2019-04-18 17:59:39 -0500

    Review fixes:
    - re-structure gitlab yaml to have one job per env and remove stages
    - remove complexity from build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
 set -ex
 
-# branch names don't correspond 1:1 with config file names
-branch="$1"
-case branch in
-    --dev)
-        SITE="dev"
-        ;;
-    --master)
-        SITE="stage"
-        ;;
-    --prod)
-        SITE="prod"
-        ;;
-    *)
-        SITE=""
-        ;;
-esac
-
 cd "$(dirname ${BASH_SOURCE[0]})"/..
 mkdir -p ./release
 
-if [[ -n $SITE ]]; then
-    # let gulp build the assets
-    docker run --rm --label=gulp \
-        --volume=$(pwd):/srv \
-        --entrypoint /bin/bash \
-        huli/gulp \
-        -c "/entrypoint.sh build --production && chown $(id -u):$(id -g) . -R"
+# let gulp build the assets
+docker run --rm --label=gulp \
+    --volume=$(pwd):/srv \
+    --entrypoint /bin/bash \
+    huli/gulp \
+    -c "/entrypoint.sh build --production && chown $(id -u):$(id -g) . -R"
 
-    # and then let hugo build the site
-    docker run --rm --label=hugo \
-        --volume=$(pwd):/src \
-        --volume=$(pwd)/release:/tmp/build \
-        -e HUGO_ENV=production \
-        jojomi/hugo:0.40.2 \
-        ash -c "rm -rf /tmp/build/* && hugo --config config.toml,config-${SITE}.toml --destination /tmp/build"
-fi
+# and then let hugo build the site
+docker run --rm --label=hugo \
+    --volume=$(pwd):/src \
+    --volume=$(pwd)/release:/tmp/build \
+    -e HUGO_ENV=production \
+    jojomi/hugo:0.40.2 \
+    ash -c "rm -rf /tmp/build/* && hugo --config config.toml,config-$1.toml --destination /tmp/build"
